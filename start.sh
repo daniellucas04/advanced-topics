1645cb59a56f428fdddc5e07ad33c446d1414d29
unknown <daniellsilva@outlook.com>
2025-09-05 21:53:22 -0300

    fix

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ chown -R mysql:mysql /var/lib/mysql
 rm -rf /var/lib/mysql/*
 
 echo "Inicializando banco de dados no volume..."
-mysqld --initialize--insecure --user=mysql --datadir=/var/lib/mysql
+mysqld --initialize --user=mysql --datadir=/var/lib/mysql
 
 # Inicia o MySQL
 service mysql start
