2d53304537eea996e08ae1d872abab39b8e6ee35
unknown <daniellsilva@outlook.com>
2025-09-11 21:41:55 -0300

    fix

--- app/Connection/Database.php
+++ app/Connection/Database.php
@@ -19,7 +19,7 @@ class Database {
         $this->password = $_ENV['DB_PASSWORD'];
 
         try {
-            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbName};", $this->username, $this->password);
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbName};charset=utf8mb4", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch (PDOException $e) {
